6d042bfb40ed80de3e856222e13561ee4d298d5d
Olivia Lin <olivia.xinyi.lin@gmail.com>
2018-12-08 16:01:52 -0800

    changed readme aabout docker and take out decision tree graph

--- src/script_03_machine_learning.py
+++ src/script_03_machine_learning.py
@@ -7,7 +7,7 @@
 # This script should be run under the root folder.
 
 # Usage: python src/script_03_machine_learning.py
-# Example: python src/script_03_machine_learning.py data/tidy_data_ml.csv results/depth_summary.csv results/feature_summary.csv results/tree_model
+# Example: python src/script_03_machine_learning.py data/tidy_data_ml.csv results/depth_summary.csv results/feature_summary.csv
 
 
 # import libraries
@@ -19,8 +19,8 @@ from sklearn.neighbors import KNeighborsClassifier, KNeighborsRegressor
 from sklearn.model_selection import cross_val_score
 from sklearn.preprocessing import normalize, scale, Normalizer, StandardScaler
 import matplotlib.pyplot as plt
-import graphviz
-from sklearn.tree import export_graphviz
+#import graphviz
+#from sklearn.tree import export_graphviz
 
 
 # parse/define command line arguments here
@@ -28,7 +28,6 @@ parser = argparse.ArgumentParser()
 parser.add_argument("input_file")
 parser.add_argument("output_file_depth")
 parser.add_argument("output_file_feature")
-parser.add_argument("output_file_tree")
 args = parser.parse_args()
 
 # loading data
@@ -73,31 +72,42 @@ feature_summary.to_csv(args.output_file_feature)
 
 # The belowing code is for creating decision tree plot
 # select features based feauture importance (for the purpose of readable decision tree graph)
-X_selected = X.ix[:,['married', 'capitalGain', 'education']]
-# spliting the data based after selected features
-X_train, X_test, y_train, y_test = train_test_split(X_selected, y, test_size=0.2, random_state = 42)
-# standardizing agian for the new data
-scaler = StandardScaler()
-scaler.fit(X_train)
-X_train = scaler.transform(X_train)
-X_test = scaler.transform(X_test)
-# fit the model again (using depth 6 for readable decision tree graph)
-model = tree.DecisionTreeClassifier(max_depth = 6, random_state = 222)
-model.fit(X_train, y_train)
-# function for sace the tree (from 571 lecture)
-def save_decision_tree(model,save_file_prefix = args.output_file_tree, **kwargs):
-    """
-    Save the decision tree model as a png
-    """
-    feature_cols = ['married', 'capitalGain', 'education']
-    class_names = ['low income','high income']
-    dot_data = export_graphviz(model, out_file = None,
-                             feature_names = feature_cols,
-                             class_names = class_names,
-                             filled = True, rounded = True,
-                             special_characters = True, **kwargs)
 
-    graph = graphviz.Source(dot_data, format = "png")
-    graph.render(save_file_prefix)
+#X_selected = X.ix[:,['married', 'capitalGain', 'education']]
+
+# spliting the data based after selected features
+
+#X_train, X_test, y_train, y_test = train_test_split(X_selected, y, test_size=0.2, random_state = 42)
+
+# standardizing agian for the new data
+
+#scaler = StandardScaler()
+#scaler.fit(X_train)
+#X_train = scaler.transform(X_train)
+#X_test = scaler.transform(X_test)
+
+# fit the model again (using depth 6 for readable decision tree graph)
+
+#model = tree.DecisionTreeClassifier(max_depth = 6, random_state = 222)
+#model.fit(X_train, y_train)
+
+# function for sace the tree (from 571 lecture)
+
+#def save_decision_tree(model,save_file_prefix = args.output_file_tree, **kwargs):
+#    """
+#    Save the decision tree model as a png
+#    """
+#    feature_cols = ['married', 'capitalGain', 'education']
+#    class_names = ['low income','high income']
+#    dot_data = export_graphviz(model, out_file = None,
+#                             feature_names = feature_cols,
+#                             class_names = class_names,
+#                             filled = True, rounded = True,
+#                             special_characters = True, **kwargs)
+
+#    graph = graphviz.Source(dot_data, format = "png")
+#    graph.render(save_file_prefix)
+
 # call the function
-save_decision_tree(model)
+
+#save_decision_tree(model)
